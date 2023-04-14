ce65ec7eff79378b11b33b2f4c739698d7e97686
bsas-git-vault <bernardo.souza@gmail.com>
2023-04-14 20:24:24 -0300

    PEQUENAS CORREÇÕES

--- api.py
+++ api.py
@@ -14,13 +14,15 @@ if btn:
         response = query.json()
         if response["status"] is None:
             st.error("Unidade não cadastrada / Coordenadas não disponíveis")
+
         else:
+            # DECLARANDO DUAS COLUNAS IDÊNTICAS
             col1, col2 = st.columns(2)
             col1.info(unidade)
             col2.info(query.status_code)
 
             # COMPONENTE EXPANDER COM JSON
-            with st.expander("ESPONSE"):
+            with st.expander("RESPONSE"):
                 st.json(response)
 
             # COLETANDO LAT E LONG DO JASON
@@ -31,12 +33,14 @@ if btn:
             lat = float(lat.replace("-","")) * -1
             long = float(long.replace("-","")) * -1
 
+            # UDANDO O CAMPO CODE POR TER OPÇÃO DE CPOIAR EMBUTIDA
             col1.code(body=lat)
             col2.code(body=long)
 
             # URL DE BUSCA DIRETO NO GOOGLE
             url_google = f"https://www.google.com/search?q={lat}%2C{long}"
-
+            
+            # MARKDOWN QUE GERA LINK PARA GOOGLE
             st.markdown(f"Abrir no [Google]({url_google})")
             
     except Exception as e:
